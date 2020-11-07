4ecdbcf28b4ad4379d545ab122e57e1e4ade5bfe
unknown <shishirkaji@gmail.com>
2020-11-07 23:28:31 +1100

    added the links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import React from "react";
 const App = () => {
   return (
-    <div>
-      navbar
-    </div>
-  )
-}
+    <nav>
+      <div className="logo">
+        {" "}
+        <img
+          src="https://himaausadvantage.com.au/images/app-logo.svg"
+          className="logo-size"
+        />
+      </div>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>{" "}
+        <li>
+          <a href="#">Work</a>
+        </li>{" "}
+        <li>
+          <a href="#">Projects</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default App
+export default App;
