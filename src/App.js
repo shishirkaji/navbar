4db7b55c7fef30133311d5096de609c288fa67be
unknown <shishirkaji@gmail.com>
2020-11-08 16:15:22 +1100

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { BrowserRouter, } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
+import LoggedInNavbar from "./component/LoggedInNavbar";
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
+      <LoggedInNavbar/>
     </BrowserRouter>
   );
 };
