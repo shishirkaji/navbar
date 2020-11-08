d95c8ed6d4e945d909332702980dd557427e449d
unknown <shishirkaji@gmail.com>
2020-11-08 14:05:03 +1100

    animation added to all the nav items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 const App = () => {
+  const [nav, setNav] = useState("nav-link");
+  let burger;
+  let navLinks;
+  useEffect(() => {
+    burger = document.querySelector(".burger");
+    navLinks = document.querySelectorAll(".nav-link li");
+  });
+  const clickHandler = () => {
+    nav === "nav-link" ? setNav("nav-active") : setNav("nav-link");
+    // this is to make the burger cross
+    burger.classList.toggle("toggle");
+    // this is to make the nav links appear one after another
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = ``;
+      } else
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+    });
+  };
   return (
     <nav>
       <div className="logo">
@@ -7,9 +29,10 @@ const App = () => {
         <img
           src="https://himaausadvantage.com.au/images/app-logo.svg"
           className="logo-size"
+          alt="logo"
         />
       </div>
-      <ul>
+      <ul className={`nav-link ${nav !== "nav-link" ? `nav-active` : ""}`}>
         <li>
           <a href="#">Home</a>
         </li>
@@ -23,6 +46,16 @@ const App = () => {
           <a href="#">Projects</a>
         </li>
       </ul>
+      <div
+        className="burger"
+        onClick={() => {
+          clickHandler();
+        }}
+      >
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
     </nav>
   );
 };
